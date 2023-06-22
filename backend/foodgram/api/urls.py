547d5cd150da73218a5a331d0aa35d1f25c9c9c2
Vladimir <116248196+mvolodka@users.noreply.github.com>
2023-06-23 01:05:28 +0300

    Created app api

---
+++ backend/foodgram/api/urls.py
@@ -0,0 +1,16 @@
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+from .views import IngredientViewSet, RecipeViewSet, TagViewSet
+
+app_name = 'api'
+
+router = DefaultRouter()
+
+router.register(r'ingredients', IngredientViewSet)
+router.register(r'tags', TagViewSet)
+router.register(r'recipes', RecipeViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
