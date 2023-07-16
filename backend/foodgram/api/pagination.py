d92daad6e78476d8a85088be854368dddaf813fa
Vladimir <116248196+mvolodka@users.noreply.github.com>
2023-07-16 13:45:17 +0300

    Update pagination.py

--- backend/foodgram/api/pagination.py
+++ backend/foodgram/api/pagination.py
@@ -2,4 +2,7 @@ from rest_framework.pagination import PageNumberPagination
 
 
 class CustomPagination(PageNumberPagination):
+    page_size = 6
     page_size_query_param = 'limit'
+    max_page_size = 20
+    
